84b5364166978b1055ce3d20f35f68767e9b8f22
madahaRCE <461049663@qq.com>
2023-03-06 20:53:09 +0800

    项目测试，新增文件提交测试

--- src/main/java/com/madaha/codesecone/controller/RCE.java
+++ src/main/java/com/madaha/codesecone/controller/RCE.java
@@ -1,4 +1,9 @@
 package com.madaha.codesecone.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/RCE/Runtime")
 public class RCE {
 }
