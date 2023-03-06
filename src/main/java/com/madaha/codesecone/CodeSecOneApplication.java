0792dfb2f162705a3c06cd55916092a46b6dcab3
madahaRCE <461049663@qq.com>
2023-03-06 22:06:32 +0800

    2023-03-06_21:12 新增文件提交测试

---
+++ src/main/java/com/madaha/codesecone/CodeSecOneApplication.java
@@ -0,0 +1,13 @@
+package com.madaha.codesecone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSecOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSecOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madaha/codesecone/CodeSecOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madaha.codesecone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSecOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
