26cc104f367fc2747e7b96d25175e24bb429f30b
madahaRCE <461049663@qq.com>
2023-03-07 13:29:23 +0800

    2023-03-07 13:28 - 新增文件提交

---
+++ src/main/java/com/madaha/codesecone/Service/ServiceImpl/LoginServiceImpl.java
@@ -0,0 +1,29 @@
+package com.madaha.codesecone.Service.ServiceImpl;
+
+import com.madaha.codesecone.Service.LoginService;
+import com.madaha.codesecone.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    @Override
+    public User loginService(String uname, String password) {
+
+        //验证码 验证
+
+        //账号密码验证
+
+        //返回视图
+
+        return null;
+    }
+
+
+    @Override
+    public User registService(User user) {
+
+
+        return null;
+    }
+}
