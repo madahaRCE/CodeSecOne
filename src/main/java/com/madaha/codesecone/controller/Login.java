53c93e29e5d1ee87d554b10be384a10c0a5857e4
madahaRCE <461049663@qq.com>
2023-03-07 12:56:42 +0800

    2023-03-07 12:56 - 新增文件提交

---
+++ src/main/java/com/madaha/codesecone/controller/Login.java
@@ -0,0 +1,30 @@
+package com.madaha.codesecone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//这里别用@RestController,用了你的请求会给你返回个json。这里要的是路由
+@Controller
+public class Login {
+
+    @RequestMapping("/login")
+    public String login(){
+
+        return "/login";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "/index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/index/test")
+    public String test(){
+
+        return "直接放json数据";
+    }
+
+}
