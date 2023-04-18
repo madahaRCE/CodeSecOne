bceb03148601ee1d5e492bd86d9a5558949a7f50
madahaRCE <461049663@qq.com>
2023-04-18 23:56:08 +0800

    2023-04-18 23:55 - 新增代码

--- src/main/java/com/madaha/codesecone/controller/AdvancedAttack/components/Log4j.java
+++
@@ -1,4 +0,0 @@
-package com.madaha.codesecone.controller.AdvancedAttack.components;
-
-public class Log4j {
-}
---
+++ src/main/java/com/madaha/codesecone/controller/AdvancedAttack/deserialize/CommonCollections/CommonCollections3.java
@@ -0,0 +1,4 @@
+package com.madaha.codesecone.controller.AdvancedAttack.deserialize.CommonCollections;
+
+public class CommonCollections3 {
+}
