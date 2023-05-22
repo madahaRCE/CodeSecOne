a57c46bdcda589c90bb7a158acf4984a43607e14
madahaRCE <461049663@qq.com>
2023-05-22 18:49:24 +0800

    2023-05-22 18:48 - 新增代码

--- src/main/java/com/madaha/codesecone/controller/BAC/OverPermission.java
+++ src/main/java/com/madaha/codesecone/controller/BAC/OverPermission.java
@@ -4,6 +4,10 @@ package com.madaha.codesecone.controller.BAC;
  * 越权访问（Broken Access Control，简称BAC）:
  *    1、水平越权
  *    2、垂直越权
+ *
+ * 参考范例：
+ *    org.joychou.controller.Cookies;  (java-sec-code)
+ *    com.best.hello.controller.BAC;   (Hello-java-Sec)
  */
 public class OverPermission {
 
