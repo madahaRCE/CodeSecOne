9e08695cb8217296449382208cc9ce0877ef8a60
madahaRCE <461049663@qq.com>
2023-06-01 00:37:07 +0800

    2023-06-01 00:36 - 新增代码

--- src/main/java/com/madaha/codesecone/controller/sqli/MyBatis.java
+++ src/main/java/com/madaha/codesecone/controller/sqli/MyBatis.java
@@ -3,10 +3,7 @@ package com.madaha.codesecone.controller.sqli;
 import com.madaha.codesecone.entity.User;
 import com.madaha.codesecone.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +16,49 @@ public class MyBatis {
 
     /**
      * @payload http://127.0.0.1:18888/SQLI/MyBatis/vul/id/1%20and%201=1%20#---
-     * @param id
-     * @return
      */
     @GetMapping("/vul/id/{id}")
-    public List<User> queryById1(@PathVariable String id){
-        return userMapper.queryById1(id);
+    public List<User> queryById_Vuln(@PathVariable String id){
+        return userMapper.queryById_Vuln(id);
     }
+
+    @GetMapping("/vul/search")
+    public List<User> search_Vuln(@RequestParam("q") String q){
+        return userMapper.search_Vuln(q);
+    }
+
+    @GetMapping("/vul/orderBy")
+    public List<User> orderBy_Vuln(String field, String sort){
+        return userMapper.orderBy_Vuln(field, sort);
+    }
+
+    @GetMapping("/vul/orderBy2")
+    public List<User> orderBy2_Vuln(String field){
+        return userMapper.orderBy2_Vuln(field);
+    }
+
+
+
+    // ---------------------------------------安全分割线-----------------------------------
+
+    @GetMapping("/safe/query")
+    public List<User> queryByUser_Safe(String user){
+        return userMapper.queryByUser_Safe(user);
+    }
+
+    @GetMapping("/safe/id/{id}")
+    public List<User> queryById_Safe(@PathVariable Integer id){
+        return userMapper.queryById_Safe(id);
+    }
+
+    @GetMapping("/safe/list")
+    public List<User> list() {
+        return userMapper.list();
+    }
+
+    @GetMapping("/safe/orderBy")
+    public List<User> orderBy_Safe(String field){
+        return userMapper.orderBy_Safe(field);
+    }
+
 }
--- src/main/java/com/madaha/codesecone/mapper/UserMapper.java
+++ src/main/java/com/madaha/codesecone/mapper/UserMapper.java
@@ -14,16 +14,47 @@ import java.util.List;
  */
 @Mapper
 public interface UserMapper {
+
     /**
-     * 传统xml 配置
+     * 传统 xml 的配置，这里必须对resources/mapper/mapper.xml文件进行配置，数据库查询语句的配置。
      */
-    List<User> orderBy(String field, String sort);
+    List<User> orderBy_Vuln(String field, String sort);
+
+    List<User> orderBy_Safe(String field);
+
+
+
+
+    // 使用#{}会产生报错；  是因为 "order by" 排序的字段不能做特殊处理，所以此处存在SQL注入漏洞问题。
+    @Select("select * from users order by ${field} desc")
+    List<User> orderBy2_Vuln(@Param("field") String field);
+
 
 
     /**
      * MyBatis3 提供了新的基于注解的配置，通过注解不再需要配置繁琐的xml文件
      */
+    @Select("select * from users where user like CONCAT('%', #{user}, '%')")
+    List<User> queryByUser_Safe(@Param("user") String user);
+
     @Select("select * from users where id = ${id}")
-    List<User> queryById1(@Param("id") String id);
+    List<User> queryById_Safe(@Param("id") Integer id);
+
+    @Select("select * from users where id = ${id}")
+    List<User> queryById_Vuln(@Param("id") String id);
+
+
+
+    @Select("select * from users")
+    List<User> list();
+
+
+
+    // 模糊搜索，直接 '%#{q}%' 预编译拼接 会报错！！！
+    @Select("select * from users where user like '%${q}%'")
+    List<User> search_Vuln(String q);
+
+    @Select("select * from users where user like concat('%', #{q}, '%')")
+    List<User> search_Safe(String q);
 
 }
