0a5e1c49b47f9de75462212de489d061cc10e7d3
13253762638 <@Pan13273014590giaogiao~>
2026-01-05 00:51:34 +0800

    2026-01-05 00:51 - 代码修改，反序列化利用链学习与防御研究，rome反序列化链子；

---
+++ src/main/java/com/madaha/codesecone/controller/AdvancedAttack/deserialize/RomeDeserialize/ROME_JdbcRowSetImpl_PocTest.java
@@ -0,0 +1,8 @@
+package com.madaha.codesecone.controller.AdvancedAttack.deserialize.RomeDeserialize;
+
+public class ROME_JdbcRowSetImpl_PocTest {
+
+
+    public static void main(String[] args) throws Exception {
+    }
+}
